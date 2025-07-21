2d1bbb7ed3fdb5b1eb582075f6c2c46bd23bc60e
Nilson Durval <durval.nilson@gmail.com>
2025-07-21 00:23:44 -0300

    Adding bootstrapper to examples project

---
+++ src/examples/EFAcceleratorTools.Examples/Infrastructure/IoC/BootStrapper.cs
@@ -0,0 +1,44 @@
+﻿using EFAcceleratorTools.Examples.Configs;
+using EFAcceleratorTools.Examples.Infrastructure.Data.Context;
+using EFAcceleratorTools.Examples.Infrastructure.Data.Repositories.Aggregates.Courses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFAcceleratorTools.Examples.Infrastructure.IoC;
+
+public static class BootStrapper
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var appConfig = configuration.GetSection("ConsoleAppConfiguration");
+        var config = appConfig.Get<ConsoleAppConfiguration>();
+
+        #region Configuration
+
+        services.Configure<ConsoleAppConfiguration>(appConfig);
+        services.AddTransient<Program>();
+
+        #endregion
+
+        #region Context
+
+        services.AddDbContext<DataContext>(options => {
+            options.EnableSensitiveDataLogging();
+            options.UseSqlServer(config?.ConnectionConfiguration.ConnectionStrings.DefaultConnection, options =>
+            {
+                var timeout = config?.ConnectionConfiguration.MaxTimeoutInSeconds;
+                options.EnableRetryOnFailure();
+                options.CommandTimeout(timeout);
+            });
+        }, contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Scoped);
+
+        #endregion
+
+        #region Repositories
+
+        services.AddScoped<ICourseRepository, CourseRepository>();
+
+        #endregion
+    }
+}
