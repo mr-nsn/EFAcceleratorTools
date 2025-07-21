01f2a7c576fec5e3b238c8d3ac9579eabb404489
Nilson Durval <durval.nilson@gmail.com>
2025-07-21 00:28:28 -0300

    Creating program class logic to examples project

--- src/examples/EFAcceleratorTools.Examples/Program.cs
+++ src/examples/EFAcceleratorTools.Examples/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFAcceleratorTools.Examples.Configs;
+using EFAcceleratorTools.Examples.Domain.Aggregates.Courses.Selects;
+using EFAcceleratorTools.Examples.Infrastructure.Data;
+using EFAcceleratorTools.Examples.Infrastructure.Data.Context;
+using EFAcceleratorTools.Examples.Infrastructure.Data.Repositories.Aggregates.Courses;
+using EFAcceleratorTools.Examples.Infrastructure.IoC;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private readonly ConsoleAppConfiguration _configuration;
+    private readonly ICourseRepository _courseRepository;
+
+    public Program(IOptions<ConsoleAppConfiguration> configuration, ICourseRepository courseRepository)
+    {
+        _configuration = configuration.Value ?? throw new ArgumentNullException(nameof(configuration), "Configuration cannot be null.");
+        _courseRepository = courseRepository;
+    }
+
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        
+        var context = host.Services.GetRequiredService<DataContext>();
+        await DbInitializer.SeedAsync(context);
+
+        await host.Services.GetRequiredService<Program>().RunAsync();
+    }
+
+    public async Task RunAsync()
+    {
+        var courses = await _courseRepository.DynamicSelectAsync(CourseSelects.BasicFields);
+
+        var jsonSettings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore
+        };
+        Console.WriteLine(JsonConvert.SerializeObject(courses, jsonSettings));
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var config = builder.Build();
+
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.RegisterServices(config);
+            });
+    }  
+}
