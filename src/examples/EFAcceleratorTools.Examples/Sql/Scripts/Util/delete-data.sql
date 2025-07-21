0c985caae9283f927ccc89197bef5aad733ab728
Nilson Durval <durval.nilson@gmail.com>
2025-07-21 00:29:34 -0300

    Adding some util sql scripts

---
+++ src/examples/EFAcceleratorTools.Examples/Sql/Scripts/Util/delete-data.sql
@@ -0,0 +1,5 @@
+﻿delete from TB_PROFILE;
+delete from TB_INSTRUCTOR;
+delete from TB_LESSON;
+delete from TB_MODULE;
+delete from TB_COURSE;
---
+++ src/examples/EFAcceleratorTools.Examples/Sql/Scripts/Util/select-data.sql
@@ -0,0 +1,18 @@
+﻿select
+	*
+from
+	TB_COURSE a
+inner join
+	TB_MODULE c on a.SQ_COURSE = c.SQ_MODULE
+inner join
+	TB_LESSON d on c.SQ_MODULE = d.SQ_MODULE
+inner join
+	TB_INSTRUCTOR b on a.SQ_INSTRUCTOR = b.SQ_INSTRUCTOR
+inner join
+	TB_PROFILE e on b.SQ_INSTRUCTOR = e.SQ_INSTRUCTOR;
+
+select * from TB_COURSE;
+select * from TB_MODULE;
+select * from TB_LESSON;
+select * from TB_INSTRUCTOR;
+select * from TB_PROFILE;
