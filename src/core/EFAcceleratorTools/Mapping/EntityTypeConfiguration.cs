b81af08f8594a3bf8406346096860a0274efc2aa
Nilson Durval <durval.nilson@gmail.com>
2025-07-21 00:12:46 -0300

    Adding mapping extensions

---
+++ src/core/EFAcceleratorTools/Mapping/EntityTypeConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using EFAcceleratorTools.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFAcceleratorTools.Mapping;
+
+public abstract class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration where TEntity : class, IAggregateRoot
+{
+    public abstract void Map(EntityTypeBuilder<TEntity> builder);
+}
---
+++ src/core/EFAcceleratorTools/Mapping/ModelBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using EFAcceleratorTools.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace EFAcceleratorTools.Mapping
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddConfiguration<TEntity>(this ModelBuilder modelBuilder, EntityTypeConfiguration<TEntity> configuration) where TEntity : class, IAggregateRoot
+        {
+            configuration.Map(modelBuilder.Entity<TEntity>());
+        }
+
+        public static void RegisterModelsMapping(this ModelBuilder modelBuilder, Dictionary<Type, IEntityTypeConfiguration> mappings)
+        {
+            foreach (var mapping in mappings)
+            {
+                var entityType = mapping.Key;
+                var genericMethod = typeof(ModelBuilderExtensions).GetMethod(nameof(ModelBuilderExtensions.AddConfiguration));
+                var method = genericMethod!.MakeGenericMethod(entityType);
+                method!.Invoke(null, new object[] { modelBuilder, mapping.Value });
+            }
+        }
+    }
+}
