872ab8149c3fad3a047b80e2c8b48f8ecba28b7a
Nilson Durval <durval.nilson@gmail.com>
2025-07-21 00:17:15 -0300

    Adding appsettings.json and configuration class to examples project

---
+++ src/examples/EFAcceleratorTools.Examples/Configs/ConsoleAppConfiguration.cs
@@ -0,0 +1,33 @@
+﻿
+namespace EFAcceleratorTools.Examples.Configs;
+public class ConsoleAppConfiguration
+{
+    public ConnectionConfiguration ConnectionConfiguration { get; set; }
+
+    public ConsoleAppConfiguration()
+    {
+        ConnectionConfiguration = new ConnectionConfiguration();
+    }
+}
+
+public class ConnectionConfiguration
+{
+    public int MaxTimeoutInSeconds { get; set; }
+
+    public ConnectionStrings ConnectionStrings { get; set; }
+
+    public ConnectionConfiguration()
+    {
+        ConnectionStrings = new ConnectionStrings();
+    }
+}
+
+public class ConnectionStrings
+{
+    public string DefaultConnection { get; set; }
+
+    public ConnectionStrings()
+    {
+        DefaultConnection = string.Empty;
+    }
+}
